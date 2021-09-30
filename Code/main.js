6e501ccb8e5b822b217e5f54a680f3e5912ddcb2
yahbouss <yahbouss@hotmail.fr>
2021-09-30 14:36:10 +0100

    first exercise

--- Code/main.js
+++ Code/main.js
@@ -1,1 +1,22 @@
-// your code goes here
+function makeAccount(initial) {
+    var account = { balance : initial}
+
+    return {
+        balance: account.balance,
+         withdraw: function(amount) {
+              if (account.balance - amount >= 0) {
+                account.balance = account.balance - amount
+                   return 'Here is your money: $' + amount
+              }
+         return 'Insufficient funds.'
+         },              
+          deposit: function(amount) {
+            account.balance = account.balance + amount
+              return 'Your balance is: $' + balance
+         }
+    }
+}
+
+var myAccount = makeAccount(500)
+
+console.log(myAccount.balance)
